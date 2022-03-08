1d3e6c5b1636d314d8ebb0c8ed70548d4349b699
Tyhket <ketistimmy@gmail.com>
2022-03-08 11:38:52 +0100

    fix

---
+++ JsonDiffProgram/Program.cs
@@ -0,0 +1,29 @@
+using JsonDiffProgram.DiffStorage;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// create store (Dictionary) with Dependency Injection
+builder.Services.AddSingleton(typeof(IDiffStorage), typeof(DiffStorage));
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
